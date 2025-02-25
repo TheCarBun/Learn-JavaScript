e6d50b7a37ec7f6111967cfdf1231f235e93d217
thecarbun <subhopriyobadyakar1412@gmail.com>
2025-02-25 12:56:28 +0530

    Adventure Game v2

--- tutorial.js
+++ tutorial.js
@@ -1,33 +1,51 @@
 const prompt = require("prompt-sync")();
-const name = prompt("What is your name? ");
-console.log("Hello", name, "welcome to our game!");
 
-const shouldWePlay = prompt("Do you want to play? ").toLowerCase().trim();
-if (shouldWePlay === "yes" || shouldWePlay === "y") {
-  const leftOrRight = prompt(
-    "You enter a maze, do you want to go left or right? "
-  )
-    .toLowerCase()
-    .trim();
-  if (leftOrRight === "l" || leftOrRight === "left") {
-    cross = prompt("You go left and find a bridge. Do you want to cross? ")
-      .toLowerCase()
-      .trim();
-    if (cross === "y" || cross === "yes") {
-      console.log("You cross the bridge but it was weak so you fall and die!");
-      console.log("<<<<<<<< GAME OVER >>>>>>>>");
-    } else if (cross === "n" || cross === "no") {
-      console.log("You made the right choice! You live another day.");
-      console.log("<<<<<<<<< YOU WIN >>>>>>>>>");
-    } else {
-      console.log("Invalid Input!");
+const gameName = "The Lost Artifact";
+const startPoint =
+  "You are an archaeologist exploring an ancient tomb.  You've just discovered a hidden chamber";
+
+const scenario1 =
+  "The chamber is dimly lit, dust motes dancing in the faint light.  You see three passages leading further into the tomb: one to the left, one straight ahead, and one to the right.";
+const choices1 = {
+  left: ["l", "left"],
+  right: ["r", "right"],
+  straight: ["s", "straight"],
+};
+
+function handleChoice(scenario, choices) {
+  let ans;
+  let validChoice = false;
+
+  while (!validChoice) {
+    console.log(scenario, "\nChoices:");
+    for (var choice in choices) {
+      console.log(choice);
     }
-  } else if (leftOrRight === "r" || leftOrRight === "right") {
-    console.log("You fall into a pit and die!");
-    console.log("<<<<<<<< GAME OVER >>>>>>>>");
-  } else {
-    console.log("Invalid Input!");
+    ans = prompt("Pick: ").toLowerCase().trim();
+
+    for (var choice in choices) {
+      if (choices[choice].includes(ans)) {
+        validChoice = true;
+        console.log(`You picked ${choice}`);
+        return choice;
+      }
+    }
+
+    if (!validChoice) {
+      console.log("Invalid input. Please try again.");
+    }
   }
+}
+
+console.log(`------- ${gameName} -------`);
+console.log("---------------------------------");
+const name = prompt("What is your name? ");
+console.log(`Hello ${name}! Welcome to "${gameName}"`);
+
+const shouldWePlay = prompt("Do you want to begin?(y/n) ").toLowerCase().trim();
+if (shouldWePlay === "yes" || shouldWePlay === "y") {
+  console.log(startPoint);
+  scene1Ans = handleChoice(scenario1, choices1);
 } else if (shouldWePlay === "no" || shouldWePlay === "n") {
   console.log("Ok :(");
 } else {
